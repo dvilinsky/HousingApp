54bf4654e9cf92d94dc2088433bf13df83e4a15c
Daniel Vilinsky <Daniel>
2017-11-17 12:50:47 -0800

    Initial commit. Initialized Spring/gradle project

---
+++ src/main/java/edu/brandeis/housing/HousingMain.java
@@ -0,0 +1,11 @@
+package edu.brandeis.housing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HousingMain.class);
+    }
+}
