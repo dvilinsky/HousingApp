0bfbe86406b4ff2f12a8ea61393096a8c998c617
Daniel Vilinsky <Daniel>
2017-11-20 10:12:02 -0800

    Just some cleanup

--- src/main/java/edu/brandeis/housing/HousingMain.java
+++ src/main/java/edu/brandeis/housing/HousingMain.java
@@ -2,6 +2,12 @@ package edu.brandeis.housing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class HousingMain {
